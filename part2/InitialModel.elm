f5f52c1e6964a9faf68b3c5942194cde3e67a407
Piotr Grota <piotrgrota@gmail.com>
2018-04-17 14:59:33 +0200

    add types alias

--- part2/InitialModel.elm
+++ part2/InitialModel.elm
@@ -2,8 +2,8 @@ module InitialModel exposing (initialModel)
 
 
 initialModel =
-    { query = "EC 2"
-    , result =
+    {
+     result =
         [ { name = "Amazon EC2", link = "http://aws.amazon.com/documentation/ec2/?icmpid=docs_menu_internal" }
         , { name = "Amazon ECR", link = "http://aws.amazon.com/documentation/ec2/?icmpid=docs_menu_internal" }
         , { name = "Amazon Lightsail", link = "http://aws.amazon.com/documentation/ec2/?icmpid=docs_menu_internal" }
--- part2/Main.elm
+++ part2/Main.elm
@@ -2,18 +2,37 @@ module Main exposing (..)
 
 import Html exposing (..)
 import Html.Attributes exposing (..)
-import InitialModel exposing (..)
 import Html.Events exposing (onClick)
+import InitialModel exposing (..)
 
 
+type alias Model =
+    { result : List SearchResult
+    }
+
+
+type alias SearchResult =
+    { name : String
+    , link : String
+    }
+
+
+type alias Msg =
+    { operation : String
+    , name : String
+    }
+
+
+viewResults : SearchResult -> Html Msg
 viewResults result =
     li []
         [ span [] [ text result.name ]
         , a [ href result.link, class "link" ] [ text "Click for Details" ]
-        ,button [class "hide-result" , onClick {operation = "DELETE_BY_ID" ,data = result.name }] [text "x"]
+        , button [ class "hide-result", onClick { operation = "DELETE_BY_ID", name = result.name } ] [ text "x" ]
         ]
 
 
+view : Model -> Html Msg
 view model =
     let
         headerInfo =
@@ -31,15 +50,15 @@ view model =
         ]
 
 
+update : Msg -> Model -> Model
 update msg model =
     if msg.operation == "DELETE_BY_ID" then
-        {model | result =  model.result |> List.filter (\result -> result.name /= msg.data)  }
+        { model | result = model.result |> List.filter (\result -> result.name /= msg.name) }
     else
-    -- TODO if msg.operation == "DELETE_BY_ID",
-    -- then return a new model without the given ID present anymore.
         model
 
 
+main : Program Never Model Msg
 main =
     Html.beginnerProgram
         { view = view
