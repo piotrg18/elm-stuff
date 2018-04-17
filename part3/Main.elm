b013bf8d54cffd2d6805d3aba89126b2ebc71679
Piotr Grota <piotrgrota@gmail.com>
2018-04-17 20:54:53 +0200

    starting part 3

---
+++ part3/Main.elm
@@ -0,0 +1,67 @@
+module Main exposing (..)
+
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (onClick)
+import InitialModel exposing (..)
+
+
+type alias Model =
+    { result : List SearchResult
+    }
+
+
+type alias SearchResult =
+    { name : String
+    , link : String
+    }
+
+
+type alias Msg =
+    { operation : String
+    , name : String
+    }
+
+
+viewResults : SearchResult -> Html Msg
+viewResults result =
+    li []
+        [ span [] [ text result.name ]
+        , a [ href result.link, class "link" ] [ text "Click for Details" ]
+        , button [ class "hide-result", onClick { operation = "DELETE_BY_ID", name = result.name } ] [ text "x" ]
+        ]
+
+
+view : Model -> Html Msg
+view model =
+    let
+        headerInfo =
+            header []
+                [ h1 [] [ text "Web App" ]
+                , div []
+                    [ span [ class "tagline" ] [ text "This will be Experiment with Elm " ]
+                    , span [ class "yellow" ] [ text " part 3" ]
+                    ]
+                ]
+    in
+    div [ class "content" ]
+        [ headerInfo
+        , ul [ class "results" ] (List.map viewResults model.result)
+        ]
+
+
+update : Msg -> Model -> Model
+update msg model =
+    if msg.operation == "DELETE_BY_ID" then
+        { model | result = model.result |> List.filter (\result -> result.name /= msg.name) }
+    else
+        model
+
+
+main : Program Never Model Msg
+main =
+    Html.beginnerProgram
+        { view = view
+        , update = update
+        , model = initialModel
+        }
