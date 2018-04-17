af3e72a15a71a9a341241ac8eaa05c5baec571f0
Piotr Grota <piotrgrota@gmail.com>
2018-04-17 10:38:22 +0200

    elm stuff

--- Main.elm
+++ Main.elm
@@ -3,20 +3,11 @@ module Main exposing (..)
 import Html exposing (..)
 import Html.Attributes exposing (..)
 
-
+main : Html msg  
 main =
     div [ class "content" ]
         [ header []
-            [ -- TODO wrap this text in an <h1>
-              text "ElmHub"
-            , span
-                [-- TODO give this span a class="tagline" attribute.
-                 --
-                 -- HINT: look at how our <div class="content"> does this above.
-                ]
-                [{- TODO put some text in here that says:
-                    "Like GitHub, but for Elm things."
-                 -}
-                ]
+            [ h1 [] [ text "WebApp" ]
+            , span [class "tagline"] [ text "This will be Experiment with Elm" ]
             ]
         ]
