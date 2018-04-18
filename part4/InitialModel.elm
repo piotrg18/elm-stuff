16da05564702518864ad4d210571f5503639db1f
Piotr Grota <piotrgrota@gmail.com>
2018-04-18 14:52:03 +0200

    part 4 json decoders

--- part4/InitialModel.elm
+++ part4/InitialModel.elm
@@ -1,9 +1,9 @@
-module InitialModel exposing (initialModel)
+module InitialModel exposing (tempInitialModel)
 
 
-initialModel =
+tempInitialModel =
     {
-     result =
+     results =
         [ { name = "Amazon EC2", link = "http://aws.amazon.com/documentation/ec2/?icmpid=docs_menu_internal" }
         , { name = "Amazon ECR", link = "http://aws.amazon.com/documentation/ec2/?icmpid=docs_menu_internal" }
         , { name = "Amazon Lightsail", link = "http://aws.amazon.com/documentation/ec2/?icmpid=docs_menu_internal" }
--- part4/Main.elm
+++ part4/Main.elm
@@ -3,13 +3,24 @@ module Main exposing (..)
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onClick)
-import InitialModel exposing(initialModel)
+import Response exposing (json)
+import Json.Decode exposing (..)
+import Json.Decode.Pipeline exposing (..)
 
 
-type alias Model =
-    { result : List SearchResult
+type alias AmazonModel = {
+    listItem : String
+    ,results : List SearchResult
+}
+
+type alias TemporaryModel =
+    {
+    flyoutList : List AmazonModel     
     }
 
+type alias Model =
+    { results : List SearchResult
+    }
 
 type alias SearchResult =
     { name : String
@@ -17,9 +28,8 @@ type alias SearchResult =
     }
 
 
-type  Msg =
-     DeleteById String
-
+type Msg
+    = DeleteById String
 
 
 viewResults : SearchResult -> Html Msg
@@ -27,7 +37,7 @@ viewResults result =
     li []
         [ span [] [ text result.name ]
         , a [ href result.link, class "link" ] [ text "Click for Details" ]
-        , button [ class "hide-result", onClick ( DeleteById result.name ) ] [ text "x" ]
+        , button [ class "hide-result", onClick (DeleteById result.name) ] [ text "x" ]
         ]
 
 
@@ -45,17 +55,58 @@ view model =
     in
     div [ class "content" ]
         [ headerInfo
-        , ul [ class "results" ] (List.map viewResults model.result)
+        , ul [ class "results" ] (List.map viewResults model.results)
         ]
 
 
 update : Msg -> Model -> Model
 update msg model =
-    case msg of 
+    case msg of
         DeleteById name ->
-            { model | result = model.result |> List.filter (\result -> result.name /= name) }
+            { model | results = model.results |> List.filter (\result -> result.name /= name) }
+
+
+searchResultDecoder : Decoder SearchResult
+searchResultDecoder =
+    decode SearchResult
+        |> Json.Decode.Pipeline.required "text" Json.Decode.string
+        |> Json.Decode.Pipeline.required "target" Json.Decode.string
+
+temporaryDecoder : Decoder AmazonModel
+temporaryDecoder =
+    decode AmazonModel
+        |> Json.Decode.Pipeline.required "listItem" Json.Decode.string
+        |> Json.Decode.Pipeline.optional "subList" (Json.Decode.list searchResultDecoder) []
+
+responseDecoder : Decoder (List AmazonModel)
+responseDecoder = 
+    decode identity
+        |> Json.Decode.Pipeline.required "flyoutList" (Json.Decode.list temporaryDecoder) 
+
         
-        
+decodeResults: String  -> List SearchResult
+decodeResults json =
+    case decodeString responseDecoder json of
+        Ok searchResult -> 
+            let hardCodedValue = searchResult 
+                |> List.filter ( \amazonEntry -> amazonEntry.listItem == "Compute")
+                |> List.head
+            in
+                case hardCodedValue of  
+                    Just node -> node.results
+                    Nothing -> []
+
+        Err errorMessage ->
+            Debug.log(errorMessage) 
+            []
+
+
+initialModel : Model
+initialModel =
+    { results = decodeResults json
+    }
+
+
 main : Program Never Model Msg
 main =
     Html.beginnerProgram
