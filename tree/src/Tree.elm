319af0b6b97a6c8700e0b4f7a9db2abaf7a83855
Piotr Grota <piotrgrota@gmail.com>
2018-04-22 21:39:46 +0200

    added tree sample operations

--- tree/src/Tree.elm
+++ tree/src/Tree.elm
@@ -1,19 +1,38 @@
+type alias Id =
+    String
 
-type Node id a = 
-        Node id a (List(Node))
+type Node a = 
+        Node Id a (List(Node a))
 
 
-type Tree = 
-    List(Node)
+type Tree a= 
+    List(Node a)
 
-newNode : a -> b -> Node a b
+
+
+newNode : Id -> a -> Node a
 newNode id item = 
     Node id item []
 
 
-nodeValue : Node id a -> a
+nodeValue : Node a -> a
 nodeValue (Node _ item _) = 
     item
 
+insert : Id -> a -> Node a -> Node a
+insert id item (Node parentId currentItem childs) =
+     Node parentId currentItem (List.append [(newNode id item)] childs) 
 
+findNodeById : Id -> Node a -> Maybe (Node a)
+findNodeById id ((Node currentId _ children)  as node) =
+    if id == currentId then
+        Just node
+    else
+        ( children |> List.filterMap(findNodeById id)) |> List.head
 
+insertNodeAfter : Id -> Id -> a -> Node a -> Node a
+insertNodeAfter nodeId newId item ((Node currentId currentItem childs) as node) =
+    if nodeId == currentId then
+        insert newId item node
+    else
+        Node currentId currentItem (List.map(insertNodeAfter nodeId newId item) childs)
